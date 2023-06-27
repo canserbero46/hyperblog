51c110ce607c93a7a7b821b52ae878b10398fa78
canserbero46 <dhitias@unicauca.edu.co>
2023-06-26 21:41:42 -0500

    add code to find emails

---
+++ get_emails.py
@@ -0,0 +1,15 @@
+import re, json
+
+re_obj = re.compile(r"<(\S+@\S+?)>")
+emails_dict = dict()
+
+with open("mails.txt", "rt", encoding="utf-8") as file:
+    for mail in re_obj.findall(file.read()):
+        if mail not in emails_dict.keys():
+            emails_dict[mail] = 1
+        else:
+             emails_dict[mail] += 1
+             
+with open("result.txt", "wt", encoding="utf-8") as file:
+    file.write(json.dumps(emails_dict, indent=4))
+        
